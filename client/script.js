e6fffb242083eeab54734756aa6a98314d7d29b5
Nowshad Ahmed <nowshad.ahmed@hotmail.co.ukgit config user.name Nowshad10>
2022-03-19 15:12:50 +0000

    Story entry section looks better, and character count is functioning.

--- client/script.js
+++ client/script.js
@@ -1,7 +1,16 @@
 // navbar javascript
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const toggleButton = document.getElementById('toggle-button')
+const navbarLinks = document.getElementById('navbar-links')
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 });
+
+// character count 
+const characterCount = document.querySelector('#story-entry');
+characterCount.addEventListener('keyup', charCount)
+function charCount(e){
+    if(e.key){
+        document.querySelector('#current').textContent=document.querySelector('#story-entry').value.length
+    }
+}
