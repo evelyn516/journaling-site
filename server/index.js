c4874d8cd48efa4e2beab422b54d9dbc5f923916
Evie <evie.ball32@gmail.com>
2022-03-21 14:44:54 +0000

    added fetch API post method

--- server/index.js
+++ server/index.js
@@ -1,8 +1,33 @@
 const app = require('./app');
 const port = process.env.port || 3000;
 
-let allPosts = loadJSON('datafiles.json')
+
+const fs = require('fs');
+const { request } = require('http');
+let posts = fs.readFileSync('datafiles.json');
+let allposts = JSON.parse(posts);
+//add date and time
+console.log(allposts.title);
+console.log(allposts.story);
+
+function addPost() {
+/*     let title = fetch/request.params
+    let story = fetch/request.params */
+    //title = JSON.stringify('title')
+    fs.writeFile('datafiles.json', title, postNow)
+    function postNow() {
+        console.log('hello')
+    }
+}
+
+app.get('/data', (req, res) => {
+    res.json(allposts)
+  })
+
+
+
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}!`);
 });
+
