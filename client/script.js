e0d22c4223a5841ced8a3a2ee82f06391c52d666
Nowshad Ahmed <nowshad.ahmed@hotmail.co.ukgit config user.name Nowshad10>
2022-03-22 17:21:38 +0000

    Form is adding info to the body

--- client/script.js
+++ client/script.js
@@ -9,20 +9,20 @@ toggleButton.addEventListener('click', () => {
 });
 
 // character count 
-const characterCount = document.querySelector('#story-entry');
+const characterCount = document.querySelector('#storyEntry');
 characterCount.addEventListener('keyup', charCount)
 function charCount(e){
     if(e.key){
-        document.querySelector('#current').textContent=document.querySelector('#story-entry').value.length
+        document.querySelector('#current').textContent=document.querySelector('#storyEntry').value.length
     }
 }
 
 
 // Giphy search
-// let APIKey = 'ct8cd1r1ee3fUNlCSMP9VKWNg0e13CwG';
+let APIKey = 'ct8cd1r1ee3fUNlCSMP9VKWNg0e13CwG';
 
-// const giffyBtn = document.getElementById('find-gif');
-// const giffyText = document.getElementById('gif-search');
+const giffyBtn = document.getElementById('find-gif');
+const giffyText = document.getElementById('gif-search');
 
 
 // giffyBtn.addEventListener('click', (e) => {
@@ -56,10 +56,20 @@ formEl.addEventListener('submit', postStoryData)
 
 async function postStoryData(e) {
   e.preventDefault();
-  const current = new Date().toLocaleString() 
+  const current = new Date().toLocaleString();
+  let gifUrl;
+    let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKey}&limit=10&q=`;
+    let str = giffyText.value.trim();
+    url = url.concat(str)
+    await fetch(url)
+    .then(response => response.json())
+    .then(content => {
+        gifUrl = content.data[0].images.fixed_height.url
+    })
+    
   const formData = new FormData(formEl)
   const formDataSerialised = Object.fromEntries(formData)
-  const jsonObject = {...formDataSerialised, dateTime: current, comments: "", emojiCount: [0,0,0]}
+  const jsonObject = {...formDataSerialised, dateTime: current, comments: "", gifSearch: gifUrl, emojiCount: [0,0,0]}
   console.log(jsonObject)
   try{
     const response = await fetch ("http://localhost:3000/test", {
@@ -77,4 +87,47 @@ async function postStoryData(e) {
   }
 }
 
+// fetching data from backend
+fetch('http://localhost:3000/print')
+.then(resp => resp.json())
+.then(resp => {
+    console.log(resp)
+    createStory(resp)
+})
 
+let list = document.querySelector('#story-show');
+function createStory(resp) {
+    
+    resp.forEach(item => {
+        // console.log(item)
+        const li = document.createElement('li')
+        li.innerHTML = `
+        <div class="apost">
+        <h4 class="postTitle">${item.storyTitle}</h4>
+        <div class="container">
+            <p class="my-1 dateTime">${item.dateTime}</p>
+        </div>
+        <img class="gifInsert" src=${item.gifSearch} alt="a rubber duck" alt="gify">
+        <p class="theStory">${item.storyEntry}</p>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col text-center">
+                <button class="btn btn-success" style="width:100%">&#128077; ${item.emojiCount[0]}</button>
+            </div>
+            <div class="col text-center">
+            <button class="btn btn-danger" style="width:100%">&#128078; ${item.emojiCount[1]}</button>
+            </div>
+            <div class="col text-center">
+            <button class="btn btn-primary" style="width:100%">&#10084; ${item.emojiCount[2]}</button>
+            </div>
+        </div>
+        <form class="comment my-2">
+            <label for = "comments">
+            </label>
+            <input type="text" id="form-search" placeholder="Add your comment here">
+        </form>
+    </div>`
+list.prepend(li)
+    })
+}
